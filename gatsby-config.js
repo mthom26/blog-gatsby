942ded66be26620c4f361936c9f2e9b866824b75
mthom26 <mthom2626@gmail.com>
2018-05-13 11:23:45 +0100

    Add typographyjs

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    siteMetadata: {
+        title: 'Site Title from Metadata'
+    },
+    plugins: [
+        {
+            resolve: 'gatsby-plugin-typography',
+            options: {
+                pathToConfigModule: 'src/utils/typography.js'
+            }
+        }
+    ]
+};
