1f3cb70611ccda1b51ef802efc8d51daf0360733
mthom26 <mthom2626@gmail.com>
2018-05-13 11:32:28 +0100

    Add Container component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Container from '../components/Container';
 
 const About = () => {
     return (
-        <div>
+        <Container>
             <h1>About Page</h1>
-        </div>
+        </Container>
     );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Container from '../components/Container';
 
 const Index = () => {
     return (
-        <div>
+        <Container>
             <h1>Index Page</h1>
-        </div>
+        </Container>
     );
 };
 
