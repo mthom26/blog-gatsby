b8ca84d55a41882884baacd8c574fd49d6f630c6
mthom26 <mthom2626@gmail.com>
2018-05-18 13:53:18 +0100

    Add styling to blog page

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -2,16 +2,23 @@ import React from 'react';
 import Link from 'gatsby-link';
 import Img from 'gatsby-image';
 
+import styles from './BlogPost.module.css';
+
 const BlogPost = ({ postData }) => {
-  // console.log(postData);
+  console.log(postData);
   return (
-    <div>
-      <Img
-        style={{height: '35vh'}}
-        sizes={postData.coverImage.sizes}
-      />
-      <h2>{postData.title}</h2>
-      <Link to={postData.slug}>Read More</Link>
+    <div className={styles.blogpost}>
+      <div className={styles.imageContainer}>
+        <h2 className={styles.title}>{postData.title}</h2>
+        <Img
+          style={{height: '35vh'}}
+          sizes={postData.coverImage.sizes}
+        />
+      </div>
+        
+      <div className={styles.content}>
+        <Link to={postData.slug}>Read More</Link>
+      </div>
     </div>
   );
 };
