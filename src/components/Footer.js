56ea39675f079341d30694d13283b19df5f5a112
mthom26 <mthom2626@gmail.com>
2018-06-01 16:07:11 +0100

    Add Footer with icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
+import GatsbyIcon from '../images/gatsbyIcon.svg';
+import ContentfulIcon from '../images/contentfulIcon.svg';
+import GithubIcon from '../images/githubIcon.svg';
+
+import styles from './Footer.module.css';
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.footer}>
+      <div className={styles.imageDiv}>
+        <img src={GatsbyIcon} alt="Gatsby" height="35px"/>
+      </div>
+      <div className={styles.imageDiv}>
+        <img src={ContentfulIcon} alt="ContentfulIcon" height="35px"/>
+      </div>
+      <div className={styles.imageDiv}>
+        <img src={GithubIcon} alt="GithubIcon" height="35px"/>
+      </div>
+    </div>
   );
 };
 
